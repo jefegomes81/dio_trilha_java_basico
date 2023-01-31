58cdc078b78eda8a1dc1754a2881b7ffe37cd7ee
Jeferson Gomes <jefe.gomes@outlook.com>
2023-01-30 21:03:08 -0300

    aula de metodos

--- sistema-smart-Tv/src/SmartTv.java
+++ sistema-smart-Tv/src/SmartTv.java
@@ -1,6 +1,42 @@
 public class SmartTv {
    Boolean ligada=false;
    int canal=1;
-   int volume = 25; 
+   int volume = 25;
+   public void mudarCanal (int novoCanal){
+       canal = novoCanal;
+   }
+   public void aumentarCanal(){
+       canal++;
+   }
+   public void diminuirCanal(){
+    canal--;
+}
+
+   public void aumentarVolume(){
+    volume++;
+    System.out.println("Aumentar o volume para: " + volume);
+
+
+   }
+   
+   public void diminuirVolume(){
+    volume--;
+    System.out.println("Diminuindo o volume para: " + volume);
+
+
+   }
+   public void aumentarVolume(){
+    volume++;
+   }
+   public void diminuirVolume(){
+    volume--;
+   }
+
+   public void ligar(){
+       ligada=true;
+   }
+   public void desligar(){
+    ligada=false;
+   }
    
 }
